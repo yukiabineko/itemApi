e4403058c5f8db5023c96bddf0ee87829118a4d6
yukiabineko <yukiabineko@live.com>
2020-08-08 23:51:31 +0900

    リセット機能

---
+++ reset.php
@@ -0,0 +1,24 @@
+<?php
+  require 'pass.php';
+  
+  try {
+    $dbh = new PDO($dsn, $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $smt = $dbh->prepare("DELETE FROM items");
+    $smt->execute();
+    $dbh = null;
+    $files = glob('tmp/*'); 
+    foreach($files as $file){ 
+          unlink($file); 
+      }
+      
+    echo "削除しました";
+    
+  } catch (Exception $e) {
+    echo htmlspecialchars($e->getMessage(),ENT_QUOTES);
+    die();
+  }
+ 
+
+
+?>
--- showPost.php
+++ showPost.php
@@ -1,10 +1,8 @@
 <?php
   require 'find.php';
   
-  $find_data = $result;
   if(!empty($result)){
-    json_encode($result, JSON_UNESCAPED_SLASHES);
-    $json = json_encode($data,JSON_UNESCAPED_SLASHES);
+    $json = json_encode($result,JSON_UNESCAPED_SLASHES);
     print_r($json);
   }
   else{
