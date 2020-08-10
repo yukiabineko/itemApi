100717ecd2e632270deb5cbfb53191173b9f3de8
abineko2 <yuki1980426@gmail.com>
2020-08-11 07:22:51 +0900

    クロスオリジン対策

--- showPost.php
+++ showPost.php
@@ -3,6 +3,7 @@
   
   if(!empty($result)){
     $json = json_encode($result,JSON_UNESCAPED_SLASHES);
+    header("Access-Control-Allow-Origin: *");
     print_r($json);
   }
   else{
--- viewJson.php
+++ viewJson.php
@@ -3,6 +3,7 @@
 
  $data = insertData();
  $json = json_encode($data,JSON_UNESCAPED_SLASHES);
+ header("Access-Control-Allow-Origin: *");
  print_r($json);
 
 
