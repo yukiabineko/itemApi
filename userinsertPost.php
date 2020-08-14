0744e87457b2b4cb369e15385a9ce712153a65eb
abineko2 <yuki1980426@gmail.com>
2020-08-14 09:24:52 +0900

    user order

--- userinsertPost.php
+++ userinsertPost.php
@@ -3,16 +3,20 @@
   
   try {
     if(!empty($_POST)){
+      header("Access-Control-Allow-Origin: *");
+      header('Access-Control-Allow-Headers: Content-Type');
+     
+     
       $dbh = new PDO($dsn, $user, $pass);
       $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       $smt = $dbh->prepare("INSERT INTO orders(shop, num, memo, item_id)VALUES(?, ?, ?, ?)");
       $smt->bindValue(1, $_POST['shop'], PDO::PARAM_STR);
       $smt->bindValue(2, (int)$_POST['num'], PDO::PARAM_INT);
-      $smt->bindValue(3, $_POST['num'], PDO::PARAM_STR);
+      $smt->bindValue(3, $_POST['memo'], PDO::PARAM_STR);
       $smt->bindValue(4, (int)$_POST['item_id'], PDO::PARAM_INT);
       $smt->execute();
       $dbh = null;
-      header("Access-Control-Allow-Origin: *");
+     
       echo '送信しました。';
 
     }
