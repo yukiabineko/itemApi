80b2073d325dc9da0834231512aa41e28a803231
abineko2 <yuki1980426@gmail.com>
2020-08-15 12:07:08 +0900

    param change

--- userinsertPost.php
+++ userinsertPost.php
@@ -9,11 +9,13 @@
      
       $dbh = new PDO($dsn, $user, $pass);
       $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $smt = $dbh->prepare("INSERT INTO orders(shop, num, memo, item_id)VALUES(?, ?, ?, ?)");
-      $smt->bindValue(1, $_POST['shop'], PDO::PARAM_STR);
-      $smt->bindValue(2, (int)$_POST['num'], PDO::PARAM_INT);
-      $smt->bindValue(3, $_POST['memo'], PDO::PARAM_STR);
-      $smt->bindValue(4, (int)$_POST['item_id'], PDO::PARAM_INT);
+      $smt = $dbh->prepare("INSERT INTO orders(day, shop, name, num, memo, item_id)VALUES(?, ?, ?, ?, ?, ?)");
+      $smt->bindValue(1, $_POST['day'], PDO::PARAM_STR);
+      $smt->bindValue(2, $_POST['shop'], PDO::PARAM_STR);
+      $smt->bindValue(3, $_POST['name'], PDO::PARAM_STR);
+      $smt->bindValue(4, (int)$_POST['num'], PDO::PARAM_INT);
+      $smt->bindValue(5, $_POST['memo'], PDO::PARAM_STR);
+      $smt->bindValue(6, (int)$_POST['item_id'], PDO::PARAM_INT);
       $smt->execute();
       $dbh = null;
      
