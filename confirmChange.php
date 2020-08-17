37c4783b9e810bd04daeaea2b900e0437ac2bcd2
abineko2 <yuki1980426@gmail.com>
2020-08-17 10:13:16 +0900

    confirm 修正

--- confirmChange.php
+++ confirmChange.php
@@ -6,20 +6,19 @@
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   if(!empty($_POST)){
    $smt = $dbh->prepare("UPDATE orders SET confirm=? WHERE id=?");
-   if($_POST['confirm'] == '1'){
-    $smt->bindValue(1, '0',PDO::PARAM_STR);
-    $smt->bindValue(2, (int)$_POST['id'], PDO::PARAM_INT);
-    $smt->execute();
-    echo '発注数を確定しました。'
-   }
-   else{
+   if($_POST['confirm'] == '0'){
     $smt->bindValue(1, '1',PDO::PARAM_STR);
     $smt->bindValue(2, (int)$_POST['id'], PDO::PARAM_INT);
     $smt->execute();
-    echo '発注数をリセットしました。'
+    echo '発注数を確定しました。';
+   }
+   else if($_POST['confirm'] == '1'){
+    $smt->bindValue(1, '0',PDO::PARAM_STR);
+    $smt->bindValue(2, (int)$_POST['id'], PDO::PARAM_INT);
+    $smt->execute();
+    echo '発注数をリセットしました。';
    }
  
-   $result = $smt->fetch(PDO::FETCH_ASSOC);
   }
  
   $dbh = null;
