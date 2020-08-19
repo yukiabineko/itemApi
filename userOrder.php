7d316edfbbf77056a3f3d2e0e34201719573a251
abineko2 <yuki1980426@gmail.com>
2020-08-19 17:40:00 +0900

    axios対策

--- userOrder.php
+++ userOrder.php
@@ -3,20 +3,21 @@
  try {
   $dbh = new PDO($dsn, $user, $pass);
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+ 
  
   if(!empty($_POST)){
     //ユーザーの情報取得
-   $smt = $dbh->prepare("SELECT * FROM users WHERE id=?");
-   $smt->bindValue(1, (int)$_POST['id'],PDO::PARAM_INT);
-   $smt->execute();
-   $user = $smt->fetch(PDO::FETCH_ASSOC);
-   $user_id = $user['id'];
+    $smt = $dbh->prepare("SELECT*FROM users WHERE email=? and password=?");
+    $smt->bindValue(1, $_POST['email'],PDO::PARAM_STR);
+    $smt->bindValue(2, $_POST['password'],PDO::PARAM_STR);
+    $smt->execute();
+    $user = $smt->fetch(PDO::FETCH_ASSOC);
+    $user_id = $user['id'];
 
    /***********user関連 *****************************************************************/
    //商品-欄
    $smt2 = $dbh->query("SELECT * from items");
-   $items = $smt2->fetch(PDO::FETCH_ASSOC);
+   $items = $smt2->fetchAll(PDO::FETCH_ASSOC);
 
    /***********item関連 *****************************************************************/
    //このアイテム一覧に該当オーダーなければ作成
@@ -26,16 +27,22 @@
      $smt3->bindValue(1, (int)$item['id'], PDO::PARAM_INT);
      $smt3->bindValue(2, (int)$user_id, PDO::PARAM_INT);
      $smt3->execute();
-     $order = $smt->fetch(PDO::FETCH_ASSOC);
+     $order = $smt3->fetch(PDO::FETCH_ASSOC);
+  
      if(empty($order)){
-      $smt4 = $dbh->prepare("INSERT INTO orders(name, item_id, user_id)VALUES(?,?,?)");
+      $smt4 = $dbh->prepare("INSERT INTO orders(name, price, item_id, user_id)VALUES(?,?,?,?)");
       $smt4->bindValue(1, $item['name'], PDO::PARAM_STR);
-      $smt4->bindValue(2, (int)$item['id'], PDO::PARAM_INT);
-      $smt4->bindValue(3, (int)$user_id, PDO::PARAM_INT);
+      $smt4->bindValue(2, (int)$item['price'], PDO::PARAM_INT);
+      $smt4->bindValue(3, (int)$item['id'], PDO::PARAM_INT);
+      $smt4->bindValue(4,(int)$user_id, PDO::PARAM_INT);
       $smt4->execute();
 
      }
    }
+   $json = json_encode($user,JSON_UNESCAPED_SLASHES);
+   header("Access-Control-Allow-Origin: *"); 
+   print_r($json);
+  
  
   }
   else{
