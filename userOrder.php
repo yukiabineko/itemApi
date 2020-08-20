04be72850f93a1397caed275154edb1122e38f1e
abineko2 <yuki1980426@gmail.com>
2020-08-21 07:15:15 +0900

    update order

--- userOrder.php
+++ userOrder.php
@@ -21,35 +21,43 @@
 
    /***********item関連 *****************************************************************/
    //このアイテム一覧に該当オーダーなければ作成
+   if(!empty($user_id) || !$user_id === 0){
+    foreach($items as $item){
+      $smt3 =$dbh->prepare("SELECT * FROM orders WHERE item_id=? AND user_id=?");
+      $smt3->bindValue(1, (int)$item['id'], PDO::PARAM_INT);
+      $smt3->bindValue(2, (int)$user_id, PDO::PARAM_INT);
+      $smt3->execute();
+      $order = $smt3->fetch(PDO::FETCH_ASSOC);
    
-   foreach($items as $item){
-     $smt3 =$dbh->prepare("SELECT * FROM orders WHERE item_id=? AND user_id=?");
-     $smt3->bindValue(1, (int)$item['id'], PDO::PARAM_INT);
-     $smt3->bindValue(2, (int)$user_id, PDO::PARAM_INT);
-     $smt3->execute();
-     $order = $smt3->fetch(PDO::FETCH_ASSOC);
-  
-     if(empty($order)){
-      $smt4 = $dbh->prepare("INSERT INTO orders(name, price, item_id, user_id)VALUES(?,?,?,?)");
-      $smt4->bindValue(1, $item['name'], PDO::PARAM_STR);
-      $smt4->bindValue(2, (int)$item['price'], PDO::PARAM_INT);
-      $smt4->bindValue(3, (int)$item['id'], PDO::PARAM_INT);
-      $smt4->bindValue(4,(int)$user_id, PDO::PARAM_INT);
-      $smt4->execute();
-
-     }
-   }
-   /******************************送信データ****************************************** */
-   $smt5 = $dbh->prepare("SELECT * FROM orders INNER JOIN items ON orders.item_id=items.id WHERE user_id=?");
-   $smt5->bindValue(1, (int)$user_id, PDO::PARAM_INT);
-   $smt5->execute();
-   $result = $smt5->fetchAll(PDO::FETCH_ASSOC);
-   $json = json_encode($result,JSON_UNESCAPED_SLASHES);
-   header("Access-Control-Allow-Origin: *");
-   print_r($json);
-   
-
+      if(empty($order)){
+       $smt4 = $dbh->prepare("INSERT INTO orders(name, price, item_id, user_id)VALUES(?,?,?,?)");
+       $smt4->bindValue(1, $item['name'], PDO::PARAM_STR);
+       $smt4->bindValue(2, (int)$item['price'], PDO::PARAM_INT);
+       $smt4->bindValue(3, (int)$item['id'], PDO::PARAM_INT);
+       $smt4->bindValue(4,(int)$user_id, PDO::PARAM_INT);
+       $smt4->execute();
  
+      }
+    }
+    /******************************送信データ****************************************** */
+    $smt5 = $dbh->prepare("SELECT * FROM orders INNER JOIN items ON orders.item_id=items.id WHERE user_id=?");
+    $smt5->bindValue(1, (int)$user_id, PDO::PARAM_INT);
+    $smt5->execute();
+    $result = $smt5->fetchAll(PDO::FETCH_ASSOC);
+
+    $jsonArray = array();
+    /*$user_json = json_encode($user,JSON_UNESCAPED_SLASHES);
+    $json = json_encode($result,JSON_UNESCAPED_SLASHES);*/
+    array_push($jsonArray,$user);
+    array_push($jsonArray, $result);
+    $send = json_encode($jsonArray,JSON_UNESCAPED_SLASHES);
+    header("Access-Control-Allow-Origin: *");
+    print_r($send);
+    
+ 
+  
+   }
+   
   }
   else{
     echo 'データ送信されてません。';
