428606175f4122d4ac3e43b5f7f5d13237fc6e96
abineko2 <yuki1980426@gmail.com>
2020-08-25 12:18:46 +0900

    アイテムデータ編集

--- imageUpdate.php
+++ imageUpdate.php
@@ -1,9 +1,24 @@
 <?php
- 
-  $imgid = (int)$_POST['id'];
+  require 'find.php';
+
+  try {
+    $dbh = new PDO($dsn, $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $smt = $dbh->prepare("SELECT * FROM items WHERE id=?");
+    $smt->bindValue(1, (int)$_GET['id'],PDO::PARAM_INT);
+    $smt->execute();
+    $result = $smt->fetch(PDO::FETCH_ASSOC);
+    $dbh = null;
+    
+  } catch (Exception $e) {
+    echo htmlspecialchars($e->getMessage(),ENT_QUOTES);
+    die();
+  }
+  unlink("tmp/data".$result['id'].".jpg");
   $data = file_get_contents("php://input");
-  $fp = fopen("tmp/data".$imgid.".jpg", 'wb');
-  chmod("tmp/data".$id.".jpg", 0777);
+  $fp = fopen("tmp/data".$result['id'].".jpg", 'wb');
+  chmod("tmp/data".$result['id'].".jpg", 0777);
   fwrite($fp, $data);
   fclose($fp);
+  print_r($result);
 ?>
--- itemUpdate.php
+++ itemUpdate.php
@@ -4,12 +4,12 @@
    $dbh = new PDO($dsn, $user, $pass);
    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-   if(!empty){
-     $smt = $dbh->prepare("UPDATE items SET items name=?, price=?, info=? WHERE id=?");
-     $smt->bindValue(1, $_POST['name'],PDO::PARAM_STR);
+   if(!empty($_POST)){
+     $smt = $dbh->prepare("UPDATE items SET name=?, price=?, info=? WHERE id=?");
+     $smt->bindValue(1, $_POST['itemsName'],PDO::PARAM_STR);
      $smt->bindValue(2, (int)$_POST['price'],PDO::PARAM_INT);
-     $smt->bindValue(3, $_POST['info'],PDO::PARAM_STR);
-     $smt->bindValue(1, (int)$_POST['id'],PDO::PARAM_INT);
+     $smt->bindValue(3, $_POST['memo'],PDO::PARAM_STR);
+     $smt->bindValue(4, (int)$_POST['id'],PDO::PARAM_INT);
      $smt->execute();
      echo '編集しました。';
    }
